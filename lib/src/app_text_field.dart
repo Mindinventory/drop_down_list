2a26d5ccb749298b34bddb816e02b4e3b648db44
Sahil Hamirani <sahil.hamirani@mindinventory.com>
2022-07-29 19:57:37 +0530

    fix(#2): Change background color.

---
+++ lib/src/app_text_field.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+import 'drop_down.dart';
+
+/// This is search text field class.
+class AppTextField extends StatefulWidget {
+  DropDown dropDown;
+  Function(String) onTextChanged;
+  VoidCallback onClearTap;
+
+  AppTextField({required this.dropDown, required this.onTextChanged, required this.onClearTap, Key? key})
+      : super(key: key);
+
+  @override
+  State<AppTextField> createState() => _AppTextFieldState();
+}
+
+class _AppTextFieldState extends State<AppTextField> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: TextFormField(
+        controller: widget.dropDown.searchController,
+        cursorColor: Colors.black,
+        onChanged: (value) {
+          widget.onTextChanged(value);
+        },
+        decoration: InputDecoration(
+          filled: true,
+          fillColor: widget.dropDown.searchBackgroundColor,
+          contentPadding: const EdgeInsets.only(left: 0, bottom: 0, top: 0, right: 15),
+          hintText: widget.dropDown.searchHintText,
+          border: const OutlineInputBorder(
+            borderSide: BorderSide(
+              width: 0,
+              style: BorderStyle.none,
+            ),
+            borderRadius: BorderRadius.all(
+              Radius.circular(8.0),
+            ),
+          ),
+          prefixIcon: const IconButton(
+            icon: Icon(Icons.search),
+            onPressed: null,
+          ),
+          suffixIcon: GestureDetector(
+            onTap: widget.onClearTap,
+            child: const Icon(
+              Icons.cancel,
+              color: Colors.grey,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/src/drop_down.dart
+++ lib/src/drop_down.dart
@@ -1,24 +1,23 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class DropDown {
-  /// This gives the button text or it sets default text as 'click me'.
-  final String? buttonText;
+import 'app_text_field.dart';
 
+class DropDown {
   /// This gives the bottom sheet title.
-  final String? bottomSheetTitle;
+  final String bottomSheetTitle;
 
   /// This will give the submit button text.
-  final String? submitButtonText;
+  final String submitButtonText;
 
   /// This will give the submit button background color.
-  final Color? submitButtonColor;
+  final Color submitButtonColor;
 
   /// This will give the hint to the search text filed.
-  final String? searchHintText;
+  final String searchHintText;
 
   /// This will give the background color to the search text filed.
-  final Color? searchBackgroundColor;
+  final Color searchBackgroundColor;
 
   /// This will give the default search controller to the search text field.
   final TextEditingController searchController;
@@ -32,27 +31,31 @@ class DropDown {
   /// This will give the call back to the selected item (single) from list.
   final Function(String)? selectedItem;
 
-  /// This will give selection choise for single or multiple for list.
+  /// This will give selection choice for single or multiple for list.
   final bool enableMultipleSelection;
+
+  /// This will set the background color to the dropdown.
+  final Color dropDownBackgroundColor;
 
   DropDown({
     Key? key,
-    this.buttonText,
-    this.bottomSheetTitle,
-    this.submitButtonText,
-    this.submitButtonColor,
-    this.searchHintText,
-    this.searchBackgroundColor,
-    required this.searchController,
     required this.dataList,
+    required this.searchController,
+    this.enableMultipleSelection = false,
+    this.bottomSheetTitle = 'Title',
+    this.submitButtonText = 'Done',
+    this.submitButtonColor = Colors.blue,
+    this.searchHintText = 'Search',
+    this.searchBackgroundColor = Colors.black12,
     this.selectedItems,
     this.selectedItem,
-    required this.enableMultipleSelection,
+    this.dropDownBackgroundColor = Colors.transparent,
   });
 }
 
 class DropDownState {
   DropDown dropDown;
+
   DropDownState(this.dropDown);
 
   /// This gives the bottom sheet widget.
@@ -80,9 +83,9 @@ class DropDownState {
 class SelectedListItem {
   bool isSelected;
   String name;
-  String ?value;
+  String? value;
 
-  SelectedListItem( this.isSelected,  this.name, {this.value});
+  SelectedListItem(this.isSelected, this.name, {this.value});
 }
 
 /// This is main class to display the bottom sheet body.
@@ -116,16 +119,14 @@ class _MainBodyState extends State<MainBody> {
         return Column(
           children: <Widget>[
             Padding(
-              padding:
-                  const EdgeInsets.only(left: 15.0, right: 15.0, top: 10.0),
+              padding: const EdgeInsets.only(left: 15.0, right: 15.0, top: 10.0),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   /// Bottom sheet title text
                   Text(
-                    widget.dropDown.bottomSheetTitle ?? 'Title',
-                    style: const TextStyle(
-                        fontWeight: FontWeight.bold, fontSize: 20.0),
+                    widget.dropDown.bottomSheetTitle,
+                    style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
                   ),
                   Expanded(
                     child: Container(),
@@ -138,30 +139,25 @@ class _MainBodyState extends State<MainBody> {
                       alignment: Alignment.centerRight,
                       child: ElevatedButton(
                           onPressed: () {
-                            List<SelectedListItem> selectedList = widget
-                                .dropDown.dataList
-                                .where((element) => element.isSelected == true)
-                                .toList();
+                            List<SelectedListItem> selectedList =
+                                widget.dropDown.dataList.where((element) => element.isSelected == true).toList();
                             List<String> selectedNameList = [];
 
                             for (var element in selectedList) {
-                              selectedNameList.add(element.value ??  element.name);
+                              selectedNameList.add(element.value ?? element.name);
                             }
 
-                            widget.dropDown.selectedItems
-                                ?.call(selectedNameList);
+                            widget.dropDown.selectedItems?.call(selectedNameList);
                             _onUnfocusKeyboardAndPop();
                           },
                           style: ElevatedButton.styleFrom(
-                            primary: widget.dropDown.submitButtonColor ??
-                                Colors.blue,
+                            primary: widget.dropDown.submitButtonColor,
                             textStyle: const TextStyle(
                               fontSize: 16,
                               fontWeight: FontWeight.bold,
                             ),
                           ),
-                          child:
-                              Text(widget.dropDown.submitButtonText ?? 'Done')),
+                          child: Text(widget.dropDown.submitButtonText)),
                     ),
                   ),
                 ],
@@ -169,7 +165,7 @@ class _MainBodyState extends State<MainBody> {
             ),
 
             /// A [TextField] that displays a list of suggestions as the user types with clear button.
-            _AppTextField(
+            AppTextField(
               dropDown: widget.dropDown,
               onTextChanged: _buildSearchList,
               onClearTap: _onClearTap,
@@ -183,7 +179,7 @@ class _MainBodyState extends State<MainBody> {
                 itemBuilder: (context, index) {
                   return InkWell(
                     child: Container(
-                      color: Colors.white,
+                      color: widget.dropDown.dropDownBackgroundColor,
                       child: Padding(
                         padding: const EdgeInsets.fromLTRB(20, 10, 20, 0),
                         child: ListTile(
@@ -194,14 +190,12 @@ class _MainBodyState extends State<MainBody> {
                               ? GestureDetector(
                                   onTap: () {
                                     setState(() {
-                                      mainList[index].isSelected =
-                                          !mainList[index].isSelected;
+                                      mainList[index].isSelected = !mainList[index].isSelected;
                                     });
                                   },
                                   child: mainList[index].isSelected
                                       ? const Icon(Icons.check_box)
-                                      : const Icon(
-                                          Icons.check_box_outline_blank),
+                                      : const Icon(Icons.check_box_outline_blank),
                                 )
                               : const SizedBox(
                                   height: 0.0,
@@ -213,12 +207,8 @@ class _MainBodyState extends State<MainBody> {
                     onTap: widget.dropDown.enableMultipleSelection
                         ? null
                         : () {
-                            widget.dropDown.selectedItem
-                                ?.call(
-                              (mainList[index].value != null) ?
-                                mainList[index].value ?? '' :
-                                  mainList[index].name
-                            );
+                            widget.dropDown.selectedItem?.call(
+                                (mainList[index].value != null) ? mainList[index].value ?? '' : mainList[index].name);
                             _onUnfocusKeyboardAndPop();
                           },
                   );
@@ -234,8 +224,7 @@ class _MainBodyState extends State<MainBody> {
   /// This helps when search enabled & show the filtered data in list.
   _buildSearchList(String userSearchTerm) {
     final results = widget.dropDown.dataList
-        .where((element) =>
-            element.name.toLowerCase().contains(userSearchTerm.toLowerCase()))
+        .where((element) => element.name.toLowerCase().contains(userSearchTerm.toLowerCase()))
         .toList();
     if (userSearchTerm.isEmpty) {
       mainList = widget.dropDown.dataList;
@@ -256,65 +245,5 @@ class _MainBodyState extends State<MainBody> {
   _onUnfocusKeyboardAndPop() {
     FocusScope.of(context).unfocus();
     Navigator.of(context).pop();
-  }
-}
-
-/// This is search text field class.
-class _AppTextField extends StatefulWidget {
-  DropDown dropDown;
-  Function(String) onTextChanged;
-  VoidCallback onClearTap;
-
-  _AppTextField(
-      {required this.dropDown,
-      required this.onTextChanged,
-      required this.onClearTap,
-      Key? key})
-      : super(key: key);
-
-  @override
-  State<_AppTextField> createState() => _AppTextFieldState();
-}
-
-class _AppTextFieldState extends State<_AppTextField> {
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(12.0),
-      child: TextFormField(
-        controller: widget.dropDown.searchController,
-        cursorColor: Colors.black,
-        onChanged: (value) {
-          widget.onTextChanged(value);
-        },
-        decoration: InputDecoration(
-          filled: true,
-          fillColor: widget.dropDown.searchBackgroundColor ?? Colors.black12,
-          contentPadding:
-              const EdgeInsets.only(left: 0, bottom: 0, top: 0, right: 15),
-          hintText: widget.dropDown.searchHintText ?? 'Search',
-          border: const OutlineInputBorder(
-            borderSide: BorderSide(
-              width: 0,
-              style: BorderStyle.none,
-            ),
-            borderRadius: BorderRadius.all(
-              Radius.circular(8.0),
-            ),
-          ),
-          prefixIcon: const IconButton(
-            icon: Icon(Icons.search),
-            onPressed: null,
-          ),
-          suffixIcon: GestureDetector(
-            onTap: widget.onClearTap,
-            child: const Icon(
-              Icons.cancel,
-              color: Colors.grey,
-            ),
-          ),
-        ),
-      ),
-    );
   }
 }

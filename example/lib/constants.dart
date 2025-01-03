5ffab886918e65836e8f61877fbb327127d35ac6
mohit chauhan <mohit.chauhan@mindinventory.com>
2025-01-03 17:57:11 +0530

    Added generic support for SelectedListItem to allow more flexible data handling.

--- example/lib/constants.dart
+++ example/lib/constants.dart
@@ -17,16 +17,44 @@ const kMonteCarlo = 'Monte Carlo';
 const kPuebla = 'Puebla';
 const kFlorence = 'Florence';
 
+// Language Name
+const kEnglish = 'English';
+const kSpanish = 'Spanish';
+const kFrench = 'French';
+const kGerman = 'German';
+const kChinese = 'Chinese';
+const kHindi = 'Hindi';
+const kArabic = 'Arabic';
+const kRussian = 'Russian';
+const kJapanese = 'Japanese';
+const kPortuguese = 'Portuguese';
+const kItalian = 'Italian';
+const kKorean = 'Korean';
+
+// Language Code
+const kEn = 'en';
+const kEs = 'es';
+const kFr = 'fr';
+const kDe = 'de';
+const kZh = 'zh';
+const kHi = 'hi';
+const kAr = 'ar';
+const kRu = 'ru';
+const kJa = 'ja';
+const kPt = 'pt';
+const kIt = 'it';
+const kKo = 'ko';
+
 // Register Page
 const kRegister = 'Register';
-const kFullName = 'Full Name';
+const kName = 'Name';
 const kEnterYourName = 'Enter your name';
 const kEmail = 'Email';
 const kEnterYourEmail = 'Enter your email';
-const kPhoneNumber = 'Phone Number';
-const kEnterYourPhoneNumber = 'Enter your phone number';
 const kCity = 'City';
 const kChooseYourCity = 'Choose your city';
+const kLanguage = 'Language';
+const kChooseYourLanguage = 'Choose your language';
 const kPassword = 'Password';
 const kAddYourPassword = 'Add your password';
 const kREGISTER = 'REGISTER';
@@ -35,3 +63,4 @@ const kREGISTER = 'REGISTER';
 const kDone = 'Done';
 const kSearch = 'Search...';
 const kCities = 'Cities';
+const kLanguages = 'Languages';
--- lib/model/selected_list_item.dart
+++ lib/model/selected_list_item.dart
@@ -1,18 +1,14 @@
 /// This is a model class used to represent an item in a selectable list
-class SelectedListItem {
+class SelectedListItem<T> {
   /// Indicates whether the item is selected
   /// Default Value: [false]
   bool isSelected;
 
-  /// The name or label of the item
-  String name;
-
-  /// An optional value associated with the item
-  String? value;
+  /// Tha data of the item
+  final T data;
 
   SelectedListItem({
-    required this.name,
-    this.value,
+    required this.data,
     this.isSelected = false,
   });
 }
